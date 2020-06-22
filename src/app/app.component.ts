bf5232c69fb3cbe1e3f4e8e78caed8535d1096e0
savitha.ramanatha <savitha.ramanatha@LSLKRT-019>
2020-06-22 13:58:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from "@angular/platform-browser";
+import{PeriodicElement} from '../app/periodicelement'
+export interface VehicleData {
+  store: string;
+  year: number;
+  make: number;
+  model: number;
+  Appraisal: number;
+}
+
+const logoURL = 
+"../assets/images/EchoPark Logo@2x.png";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-material';
+  constructor (
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+          this.matIconRegistry.addSvgIcon(
+      "logo",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(logoURL));
+    }
+      
+    dataSource: VehicleData[] = [
+      {store: 'Yogurt', year: 159, make: 6, model: 24, Appraisal: 4},
+      {store: 'Sandwich', year: 237, make: 9, model: 37, Appraisal: 4},
+      {store: 'Eclairs', year: 262, make: 16, model: 24, Appraisal: 6},
+      {store: 'Cupcakes', year: 305, make: 4, model: 67, Appraisal: 4},
+      {store: 'Gingerbreads', year: 356, make: 16, model: 49, Appraisal: 4},
+   ];
+   displayedColumns: string[] = ['store', 'year', 'make', 'model','Appraisal'];
+      
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatTableModule} from '@angular/material/table';
+import{MatSortModule} from '@angular/material/sort';
+import{MatPaginatorModule} from '@angular/material/paginator';
+import{MatInputModule} from '@angular/material/input';
+import{MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    MaterialModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
